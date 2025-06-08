5b1370c3533819826dd6777bfb8250dec1c0149d
Manuel Zarat <manuel@zarat.ml>
2025-06-08 18:43:13 +0200

    Create delete-vhost.sh

---
+++ delete-vhost.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+user=$1
+
+deluser $1
+
+rm -rf /home/$1
+
+rm -rf /etc/nginx/sites-enabled/$1.conf
+rm -rf /etc/nginx/sites-available/$1.conf
+
+docker rm -f $1
+
+rm -rf /etc/letsencrypt/live/$1.zarat.at
+
+systemctl reload vsftpd
+nginx -s reload
