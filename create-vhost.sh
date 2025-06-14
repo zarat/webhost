8aab1f88b545fe0b11530868ed84ba837ca28326
Manuel Zarat <manuel@zarat.ml>
2025-06-14 23:15:44 +0200

    Update create-vhost.sh

--- create-vhost.sh
+++ create-vhost.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 user=$1
 
-read -p 'Please specify the users domain: ' user_domain
-
-#!/bin/bash
-user=$1
-
 # add system user
 useradd -m $1
 usermod -aG www-data $1
@@ -21,23 +16,15 @@ mkdir -p /home/$1/public_html
 chmod 755 /home/$1
 chmod 775 /home/$1/public_html
 chown -R $1:$1 /home/$1
+
 chown -R www-data:$1 /home/$1/public_html
 
+# choose image
+read -p 'Choose an image (ubuntu1804php72, ubuntu2204php81, ubuntu2404php83): ' containerimage
+
 # start container
-read -p "Welches Image soll genutzt werden? (h [http] / p [php/mysql]): " antwort
-case "$antwort" in
-    [h])
-        docker run -dit --name $user --restart=always -v/home/$user/public_html:/usr/local/apache2/htdocs httpd
-        ;;
-    [p])
-        docker run -dit --name $user --restart=always -v/home/$user/public_html:/var/www/html custom_lamp
-        ;;
-    *)
-        echo "Ungültige Eingabe. Bitte j oder n eingeben."
-        ;;
-esac
-#docker run -dit --name $user --restart=always -v/home/$user/public_html:/usr/local/apache2/htdocs httpd
-#docker run -dit --name $user --restart=always -v/home/$user/public_html:/var/www/html custom_lamp
+# docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html --network macvlan_net --ip=192.168.0.2 $containerimage
+docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html $containerimage
 
 container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$user")
 
@@ -46,7 +33,7 @@ vhost_file="/etc/nginx/sites-available/$user.conf"
 cat > "$vhost_file" <<EOF
 server {
     listen 80;
-    server_name $user.$user_domain;
+    server_name $user.zarat.at;
 
     location / {
         proxy_pass http://$container_ip;
@@ -63,4 +50,4 @@ ln -s "$vhost_file" /etc/nginx/sites-enabled/
 
 nginx -s reload
 
-certbot --nginx -d $user.$user_domain --non-interactive --agree-tos -m $user@$user_domain
+certbot --nginx -d $user.zarat.at --non-interactive --agree-tos -m manuel@zarat.at
