c7acdedaa8d23f7f16f8e6f2b53f1db5d0fe12ac
Manuel Zarat <manuel@zarat.ml>
2025-06-15 14:50:43 +0200

    Update create-vhost.sh

--- create-vhost.sh
+++ create-vhost.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+echo "[info] Adding new user '$1'"
 user=$1
 
 # add system user
 useradd -m $1
 usermod -aG www-data $1
 
-# set default password
-echo "$1:password" | chpasswd
+echo "[info] Generating random password"
+# generate random password for ftp and mysql root user
+password=$(openssl rand -base64 8)
 
+echo "[info] Changing password of user"
+# set default password
+echo "$1:$password" | chpasswd
+
+echo "[info] Reloading vsftpd"
 # reload vsftpd
 systemctl reload vsftpd
 
+echo "[info] Creating folder structure and setting permissions for vhost /home/$1"
 # create user dir
 mkdir -p /home/$1/public_html
 chmod 755 /home/$1
@@ -20,16 +28,34 @@ chown -R $1:$1 /home/$1
 chown -R www-data:$1 /home/$1/public_html
 
 # choose image
-read -p 'Choose an image (ubuntu1804php72, ubuntu2204php81, ubuntu2404php83): ' containerimage
+# read -p 'Choose an image (ubuntu1804php72, ubuntu2204php81, ubuntu2404php83): ' containerimage
+containerimage="ubuntu2404php83"
+echo "[info] Using image $containerimage"
 
+echo "[info] Starting container"
 # start container
-# docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html --network macvlan_net --ip=192.168.0.2 $containerimage
-docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html $containerimage
+# docker run -dit --name $user --restart=always -v /home/$user/public_html:/usr/local/apache2/htdocs httpd
+# docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html -e MYSQL_ROOT_PASSWORD=password fauria/lamp
+# docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html test-image
+# docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html --network macvlan_net --ip=192.168.0.2 test-image
+docker run -dit --name $user --restart=always -v /home/$user/public_html:/var/www/html $containerimage > /dev/null
+
+echo "[info] Starting services in container"
+docker exec "$user" bash -c "service nginx start" > /dev/null
+docker exec "$user" bash -c "/etc/init.d/mariadb start" > /dev/null
+docker exec "$user" bash -c "service php8.3-fpm start" > /dev/null
+
+echo "[info] Changing mysql root password"
+# change mysql root password
+docker exec "$user" bash -c "mysql -u root -e \"
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$password';
+FLUSH PRIVILEGES;
+\""
 
 container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$user")
 
+echo "[info] Generating vhost configuration"
 vhost_file="/etc/nginx/sites-available/$user.conf"
-
 cat > "$vhost_file" <<EOF
 server {
     listen 80;
@@ -48,6 +74,11 @@ EOF
 
 ln -s "$vhost_file" /etc/nginx/sites-enabled/
 
+echo "[info] Reloading nginx"
 nginx -s reload
 
-certbot --nginx -d $user.zarat.at --non-interactive --agree-tos -m manuel@zarat.at
+echo "[info] Requesting LetsEncrypt certificate for $1.zarat.at"
+certbot --nginx -d $user.zarat.at --non-interactive --agree-tos -m manuel@zarat.at > /dev/null
+
+echo "User: $user"
+echo "Password: $password"
