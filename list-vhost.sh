edcb39cd233f94ce66d5fefd7d1525ef3d635ccd
Manuel Zarat <manuel@zarat.ml>
2025-06-16 15:44:59 +0200

    Update list-vhost.sh

--- list-vhost.sh
+++ list-vhost.sh
@@ -5,15 +5,25 @@ RED="\e[31m"
 GREEN="\e[32m"
 RESET="\e[0m"
 
-# Alle Container-Namen (auch gestoppte Container, falls gewünscht)
+# Alle Container-Namen (auch gestoppte Container)
 containers=$(docker ps -a --format '{{.Names}}')
 
 # Durchlaufe alle Container
 for name in $containers; do
     conf_file="/etc/nginx/sites-available/$name.conf"
+    cert_dir="/etc/letsencrypt/live/$name.zarat.at"
+
     if [[ -f "$conf_file" ]]; then
-        echo -e "${GREEN}[$name] $conf_file${RESET}"
+        echo -e "${GREEN}[$name] vHost configuration found${RESET}"
     else
-        echo -e "${RED}[$name] Keine Konfigurationsdatei gefunden${RESET}"
+        echo -e "${RED}[$name] vHost configuration NOT FOUND${RESET}"
     fi
+
+    if [[ -d $cert_dir ]]; then
+        echo -e "${GREEN}[$name] Certificate OK${RESET}"
+    else
+        echo -e "${RED}[$name] Certificate NOT FOUND${RESET}"
+    fi
+
+    echo ""
 done
