12879f4d3ae5e0f73f21142fe81c0f4f9d2038b3
Manuel Zarat <manuel@zarat.ml>
2025-06-18 10:34:43 +0200

    Update webserver.php

--- templates/webserver.php
+++ templates/webserver.php
@@ -1,5 +1,4 @@
 <?php
-include "header.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     header("Content-Type: text/plain");
@@ -18,6 +17,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo $output ?: "Keine Ausgabe vom Script.";
     exit;
 }
+else {
+    include "header.php";
+}
 ?>
 
 <!DOCTYPE html>
@@ -25,13 +27,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Free Webspace Registrierung</title>
+  <title>Webserver Registrierung</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
   <!-- <header class="bg-white shadow">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">Kostenloser Webspace</h1>
+      <h1 class="text-3xl font-bold text-gray-900">Kostenloser Webserver</h1>
     </div>
   </header> -->
 
